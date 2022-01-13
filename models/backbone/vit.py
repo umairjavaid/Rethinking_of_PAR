2cb9d38a28d6554f858cbe8c887394742de934c7
umairjavaid <bscs13024@itu.edu.pk>
2022-01-13 22:22:44 +0500

    Update vit.py

--- models/backbone/vit.py
+++ models/backbone/vit.py
@@ -28,7 +28,15 @@ import torch
 import torch.nn as nn
 
 from timm.data import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
-from torch._six import container_abcs
+
+TORCH_MAJOR = int(torch.__version__.split('.')[0])
+TORCH_MINOR = int(torch.__version__.split('.')[1])
+if TORCH_MAJOR == 1 and TORCH_MINOR < 8:
+    from torch._six import container_abcs
+else:
+    import collections.abc as container_abcs
+
+
 import torch.nn.functional as F
 import torch.utils.checkpoint as checkpoint
 
