008e01c8865c47a391985355b47fe6bea2872e2a
umairjavaid <bscs13024@itu.edu.pk>
2022-01-25 15:51:58 +0500

    Create custom_dataset.py

---
+++ custom_dataset.py
@@ -0,0 +1,37 @@
+import torch
+from torch.utils.data import Dataset
+
+class RapDataset(Dataset):
+    def __init__(self, txt_file):
+        self.data = []
+        self.img_dim = (227,227)
+        with open(txt_file) as f:
+            lines = f.read().splitlines()
+            for i in lines:
+                path = i.split(' ')[0]
+                labels = i.split(' ')[1]
+                labels_atr = labels[:][0:-2]
+                labels_atr2 = []
+                        
+                for i in labels_atr.split(","):
+                    #bad code
+                    if(i != ''):
+                        labels_atr2.append(int(i))
+                labels_view = [int(labels[-2])]
+                
+                labels_atr2 = torch.tensor(labels_atr2)
+                labels_view = torch.tensor(labels_view)
+                self.data.append([path,labels_atr2,labels_view])
+                        
+    def __len__(self):
+        return len(self.data)
+    def __getitem__(self, idx):
+        img_path, labels_atr, labels_view = self.data[idx]
+        #print(img_path, labels_atr, labels_view)
+        img = cv2.imread(img_path)
+        img = cv2.resize(img, self.img_dim)
+        img_tensor = torch.from_numpy(img)
+        img_tensor = img_tensor.permute(2, 0, 1)
+        return img_tensor, labels_atr, labels_view 
+      
+    
